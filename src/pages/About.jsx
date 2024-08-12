0c1e19bee6ec477b0a0c71779a0be890b60fd236
Japheth Dievole Cobbinah <dievole7@gmail.com>
2024-08-12 15:28:56 -0700

    Added the About Page Description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,27 +8,20 @@ const About = () => {
       <div className="container pt-14">
         <div className="py-10">
           <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
-            About us
+          Who We Are and What We Do
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            ducimus cupiditate quo harum officia perspiciatis hic ut sunt
-            repudiandae, laboriosam expedita nostrum tempora, consectetur
-            itaque, voluptate error aperiam corrupti ullam aspernatur a!
-            Dolorem, reprehenderit amet quidem deleniti iure rem vel a enim
-            ipsam hic numquam consequatur eius id eaque accusamus repudiandae
-            impedit, quisquam non, harum inventore ratione? Tempora voluptatum
-            ut eligendi corrupti esse, repellat nesciunt illum facilis officiis?
-            Nisi quidem officiis asperiores nostrum ipsa maiores explicabo quia!
-            Corporis provident asperiores fuga eligendi rem temporibus possimus
-            autem excepturi! Facilis sunt corrupti nesciunt asperiores, ab
-            consectetur doloremque, velit a mollitia possimus quaerat!
+          Everyone has different approach and priority when it comes to travel planning.
+          The team at Q1 Travel & Tours have various areas of expertise in order to cover all of our clients’ travel needs.
+          We are a Travel, Ticketing and Tourism sales Company licensed by the Ghana Tourism Authority and a member of the Tour Operators Union of  Ghana. 
+          Q1 Travel & Tours services is committed to excellence in corporate integrity, performance and overall satisfaction.
+          We are located right in the city of Accra,  Ghana.
+
           </p>
           <br />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-            suscipit minus similique aliquam recusandae quisquam id nulla
-            tempora voluptate vero.
+          Whether you’re planning an upcoming trip for work or leisure & family vacations,  we make the booking process easy and enjoyable. 
+          Call us today and start traveling!
           </p>
         </div>
       </div>
