ed43a79aed660ff61351549ace193191d4dee604
Japheth Dievole Cobbinah <dievole7@gmail.com>
2024-08-14 18:13:20 -0700

    added the google location

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -12,7 +12,7 @@ const Location = () => {
 
           <div className="rounded-xl ">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7116.040113933064!2d83.97168138953894!3d26.90285855661167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39938d631905f7c1%3A0x189dbadf0b42da46!2sNew%20Mobile%20World!5e0!3m2!1sen!2sin!4v1700902186385!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.7130464355309!2d-0.23514228320517508!3d5.588852838015892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf997e08cb9211%3A0x2cff9f042d1d455a!2sQ1%20TRAVEL%20%26%20TOURS!5e0!3m2!1sen!2sgh!4v1723684303839!5m2!1sen!2sgh"
               width="100%"
               height="360"
               allowfullscreen=""
